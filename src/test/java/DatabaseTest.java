337c54acd50613db44065e99de40f667fb32b4e3
VictorHanert <vict660m@stud.kea.dk>
2022-09-21 16:11:05 +0200

    Unit-test for deleteSuperhero()

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -7,40 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
 
-    Database database;
+    private Database database;
 
     @BeforeEach
     public void setup() {
         database = new Database();
+        database.createSuperhero("Victor Hanert", "Actionman", 2001, "Lækker", true,999);
+        database.createSuperhero("Bruce Wayne", "Batman", 1965, "Rig", true,2);
+        database.createSuperhero("Clark Kent", "Superman", 1970, "Super-evner", true,3);
+        database.createSuperhero("Tony Stark", "Iron Man", 1998, "Flyve", true,1.2);
+        database.createSuperhero("", "Black Panther", 1973, "Sort", true,5.5);
+        database.createSuperhero("Bruce Banner", "Hulk", 1967, "Stærk", false,4);
+
     }
 
     @Test
     void createOneSuperHero() {
         //Arrange
-        database.createSuperhero("Bruce Wayne", "Batman", 1939, "None", true, 2);
-        int expected = 1;
+        int expected = database.getAllSuperheroes().size() + 1;
 
-        //act
+        //Act
+        database.createSuperhero("Peter Parker", "Spider-man", 1949, "Edderkop", true, 2);
         int actual = database.getAllSuperheroes().size();
 
-        //assert
+        //Assert
         assertEquals(expected,actual);
     }
 
     @Test
     void createMultipleSuperheroes(){
-        //Arange
-        database.createSuperhero("Bruce Wayne", "Batman", 1939, "None", true, 2);
+        //Arrange
+        int expected = database.getAllSuperheroes().size() + 3;
+
+        //Act
         database.createSuperhero("Clark Kent", "Superman", 1938, "Superhuman strength", true, 4);
         database.createSuperhero("Peter Parker", "Spider-Man", 1962, "Spider-Sense", true, 3);
         database.createSuperhero("Diana Prince", "Wonder Woman", 1941, "Flight", true, 2.5);
 
-        int expected = 4;
-
-        //Act
         int actual = database.getAllSuperheroes().size();
 
-        //assert
+        //Assert
         assertEquals(expected,actual);
     }
 
@@ -50,8 +56,8 @@ class DatabaseTest {
         //Arrange
         ArrayList<Superhero> expected = new ArrayList<>(1);
 
-        //act
-        ArrayList<Superhero> actual = database.searchForSuperhero("man");
+        //Act
+        ArrayList<Superhero> actual = database.searchForSuperhero("volapyk");
 
         //Assert
         assertEquals(expected,actual);
@@ -60,14 +66,14 @@ class DatabaseTest {
     @Test
     void searchForOneSuperhero() {
         //Arrange
-        Superhero s1 = database.createSuperhero("Bruce Wayne", "Batman", 1939, "None", true, 2);
+        Superhero s1 = database.createSuperhero("Olga", "Cat-woman", 1990, "None", true, 1.2);
 
         ArrayList<Superhero> expected = new ArrayList<>(1);
 
         expected.add(s1);
 
-        //act
-        ArrayList<Superhero> actual = database.searchForSuperhero("man");
+        //Act
+        ArrayList<Superhero> actual = database.searchForSuperhero("Cat");
 
         //Assert
         assertEquals(expected,actual);
@@ -76,19 +82,32 @@ class DatabaseTest {
     @Test
     void searchForMultipleSuperheroes() {
         //Arrange
-        Superhero s1 = database.createSuperhero("Bruce Wayne", "Batman", 1939, "None", true, 2);
-        Superhero s2 = database.createSuperhero("Clark Kent", "Superman", 1938, "Superhuman strength", true, 4);
-        Superhero s3 = database.createSuperhero("Peter Parker", "Spider-Man", 1962, "Spider-Sense", true, 3);
-        ArrayList<Superhero> expected = new ArrayList<>(3);
+        int expectedSearch = 4;
 
-        expected.add(s1);
-        expected.add(s2);
-        expected.add(s3);
-
-        //act
-        ArrayList<Superhero> actual = database.searchForSuperhero("man");
+        //Act
+        int actualSearch = database.searchForSuperhero("man").size();
 
         //Assert
-        assertEquals(expected,actual);
+        assertEquals(expectedSearch,actualSearch);
+    }
+
+    @Test
+    void deleteSuperhero() {
+        //Arrange
+        ArrayList<Superhero> results = database.getAllSuperheroes();
+        Superhero superhero = database.getAllSuperheroes().get(0);
+
+        int expectedSize = results.size()-1;
+        boolean expectedResult = true;
+
+        //Act
+        ArrayList<Superhero> resultAfterDelete = database.getAllSuperheroes();
+
+        boolean actualResult = database.deleteSuperhero(superhero);
+        int actualSize = resultAfterDelete.size();
+
+        //Assert
+        assertEquals(expectedResult, actualResult);
+        assertEquals(expectedSize, actualSize);
     }
 }
